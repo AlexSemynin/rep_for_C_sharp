9383696a5c1e07824c2f7b10794746d7cadf30e7
AlexSemynin <alex.semynin97@mail.ru>
2019-01-03 22:09:39 +0300

    Создана папка с проектами для С#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿/*начало секции подключаемых пространств имен*/
+using System;
+/*конец секции подключаемых пространств имен*/
+
+namespace HelloWorld /*объявление нового пространства имен*/
+{
+    class Program /*объявление нового класса*/
+    {
+        static void Main(string[] args) /*объявление нового метода*/
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine(); /*Считываем с консоли строку*/
+            Console.WriteLine($"Привет, {name}");
+            Console.WriteLine("this is your first programs on C#, and tribute to God Programming");
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
