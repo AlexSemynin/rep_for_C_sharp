8a65b423adf312e30654b548584a2e58cca9d9ce
AlexSemynin <alex.semynin97@mail.ru>
2019-01-04 02:05:22 +0300

    Create progr for visualisation work whis variables, Завтра утром доделаю) А вообще тут только немного инициализации переменных и консольный вывод, похожий на С

---
+++ variablesAndBasicOperations/variablesAndBasicOperations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace variablesAndBasicOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hello = "Hello, World";
+            {
+                Console.Write(hello + "\n");
+                Console.Write("by, World\n");
+            }
+
+            string name = Console.ReadLine();
+            int age = 15;
+            double weight = 73.5;
+              Console.Write($"Имя : {name}\n");
+              Console.WriteLine("Возраст: {0}, Вес: {1}", age, weight);
+              Console.ReadKey();
+        }
+    }
+}
