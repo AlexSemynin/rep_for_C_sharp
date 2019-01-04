e4d9b644c47fb7ceee9760b5c054fc84dbfa68b2
AlexSemynin <alex.semynin97@mail.ru>
2019-01-04 23:38:13 +0300

    Условные конструкции

---
+++ ifWhileForList/ifWhileForList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ifWhileForList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Условные конструкции
+                int num1 = 8;
+                int num2 = 4;
+                if(num2 > num1)
+                {
+                    Console.WriteLine(num2 > num1);
+                }
+                else if(num1 > num2)
+                {
+                    Console.WriteLine(num1 > num2);
+                }
+                else
+                {
+                    Console.WriteLine("Упс");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
