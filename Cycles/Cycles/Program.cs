730a0bf02396a0f09929cc999e908f07bf798f0e
AlexSemynin <alex.semynin97@mail.ru>
2019-01-05 01:36:16 +0300

    Циклы

---
+++ Cycles/Cycles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                if(i == 2)
+                {
+                    continue;
+                }
+                Console.Write($"i = {i}, result ={i * i}\n");
+            }
+            Console.ReadKey();
+
+            // выполняется пока условие == True
+        }
+    }
+}
