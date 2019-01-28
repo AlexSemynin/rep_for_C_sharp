23ec76f3a0e8698dce16b76c5cfb71243add2494
AlexSemynin <alex.semynin97@mail.ru>
2019-01-28 23:06:16 +0300

    Инициализированна прога для задач с массивами

---
+++ SortMass/SortMass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortMass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthMass = 5;
+            int[] NameArray = new int[lengthMass];
+            for(int i = 0; i < lengthMass; i++)
+            {
+                NameArray[i] = i+1;
+            }
+            for(int i = 0; i < lengthMass; i++)
+            {
+                Console.WriteLine(NameArray[i]);
+            }
+
+            ////Многомерные массивы
+            //int[,] BigMass = new int[1, 2];
+            //int[,] BigMass1 = new int[,] { { }, { } };
+            //for(int i = 0; i < 5; i++)
+            //{
+            //    for(int j = 0; j < 4; i++)
+            //    {
+            //        BigMass1[i, j] = i;
+            //        Console.Write(BigMass1[i, j]);
+            //    }
+            //}
+            
+            //ЗАДАЧИ С МАССИВАМИ
+            Console.ReadKey();
+        }
+    }
+}
