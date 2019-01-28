382829388e8c34f683f1474771308d7cb73491f3
AlexSemynin <alex.semynin97@mail.ru>
2019-01-29 00:55:39 +0300

    Добавленна программа для методов. По сути - это функции. \n https://metanit.com/sharp/tutorial/2.8.php

---
+++ 1Method/1Method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //вызов метода
+            SayHello();
+            string str = GetBy();
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static string GetBy()
+        {
+            return "by, World";
+        }
+    }
+}
