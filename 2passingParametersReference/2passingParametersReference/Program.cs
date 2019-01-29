e9924f9cbc720ddb7956f376c1a6d184b9359e27
AlexSemynin <alex.semynin97@mail.ru>
2019-01-29 12:16:42 +0300

    Передача параметра в метод по ссылке - передатся адресс ячейки в памяти => переменная изменяется, когда в методе над ней совершаюстся операции. Вызов метода: NameMethod(ref a)           Инициализация метода static void NameMethod(ref int a){}

---
+++ 2passingParametersReference/2passingParametersReference/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2passingParametersReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 1;
+            Increment(a);
+            Console.WriteLine($"переменная по значению {a}\n");
+            IncrementREF(ref b);
+            Console.WriteLine($"переменная по ccылке в Main {b}");
+            Console.ReadKey();
+        }
+        static void Increment(int a)
+        {
+            a++;
+            Console.WriteLine(a);
+        }
+        static void IncrementREF(ref int a)
+        {
+            a++;
+            Console.WriteLine($"переменная по ссылке в методе {a}");
+        }
+    }
+}
