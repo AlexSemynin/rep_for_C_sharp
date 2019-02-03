e6062a3e8357302e327fb252d2633b2046b0329c
AlexSemynin <alex.semynin97@mail.ru>
2019-02-03 03:38:18 +0300

    Это обычный способ

--- 4_2_Properties_in_incapsul_2Method_Get_Set/4_2_Properties_in_incapsul_2Method_Get_Set/Program.cs
+++ 4_2_Properties_in_incapsul_2Method_Get_Set/4_2_Properties_in_incapsul_2Method_Get_Set/Program.cs
@@ -10,6 +10,27 @@ namespace _4_2_Properties_in_incapsul_2Method_Get_Set
     {
         static void Main(string[] args)
         {
+            Human human1 = new Human();
+            human1.SetAge(10);
+            int a = human1.GetAge();
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+    class Human
+    {
+        private int age;
+        public int GetAge()
+        {
+            return age;
+        }
+        public void SetAge(int value)
+        {
+            if (value > 0)
+            {
+                age = value;
+            }
+            else { Console.WriteLine("Введите Ваш истинный возраст"); }
         }
     }
 }
