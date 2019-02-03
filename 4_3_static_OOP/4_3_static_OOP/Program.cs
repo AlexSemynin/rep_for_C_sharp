8a508eb09111d6031ae284299a474efca75c79af
AlexSemynin <alex.semynin97@mail.ru>
2019-02-03 22:38:04 +0300

    Статические поля класса - записываются в общую память для всех экземпляров этого класса. Статические методы, свойства работают только со статическими членами класса. Тут программа по хранению количества пользователей

---
+++ 4_3_static_OOP/4_3_static_OOP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_3_static_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+            User user2 = new User();
+            user1.DisplayCountUser();
+            Console.WriteLine(User.countUser); //можно обратиться не через экземпляр класса, а через Имя класса, если поле открытое, как тут))
+            Console.ReadKey();
+        }
+    }
+    class User
+    {
+        //Статические поля применяются для хранения счетчиков
+        public static int countUser = 0;                                //Тут
+        //Конструктор по созданию пользователей
+        public User()
+        {
+            countUser += 1;
+        }
+        //Статические методы могут общаться только со статическими членами класса
+        public void DisplayCountUser()
+        {
+            Console.WriteLine($"Количество пользователей{countUser}");
+        }
+    }
+}
