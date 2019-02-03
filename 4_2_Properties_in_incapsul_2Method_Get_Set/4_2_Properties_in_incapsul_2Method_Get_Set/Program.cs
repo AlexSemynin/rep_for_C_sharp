61607438426e9ca5f6c45317869d74ecbcfdd291
AlexSemynin <alex.semynin97@mail.ru>
2019-02-03 03:27:04 +0300

    2 свойства для получения и записи данных в поле. 2метода

---
+++ 4_2_Properties_in_incapsul_2Method_Get_Set/4_2_Properties_in_incapsul_2Method_Get_Set/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_Properties_in_incapsul_2Method_Get_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
