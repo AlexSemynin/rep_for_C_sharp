e24de6fd9efa053223b904d5fa57c01b2bcb8472
AlexSemynin <alex.semynin97@mail.ru>
2019-02-06 18:34:16 +0300

    Виртуальные методы и полиморфизм на примере криков животных

---
+++ 4_4_1_HacledoBaH/4_4_1_HacledoBaH/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_1_HacledoBaH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[3];
+            Cat cat = new Cat(); cat.Name = "Barsic";
+            Dog dog = new Dog(); dog.Name = "Sharic";
+            Rat rat = new Rat(); rat.Name = "Businca";
+
+            animals[0] = cat;
+            animals[1] = dog;
+            animals[2] = rat;
+
+            for(int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine("animal " + animals[i].Name + " say:");
+                animals[i].GetRRR();
+            }
+            Console.ReadKey();
+
+
+
+            //upcast
+            Cat cat1 = new Cat();
+            cat1.Name = "Nusha";
+
+            Animal animal1 = cat1;
+            cat1.GetRRR();
+            Console.ReadKey();
+
+        }
+    }
+    //Console.WriteLine($"animal {Name} say:\n");
+    class Animal
+    {
+        public string Name { get; set; }
+        public virtual void GetRRR()
+        {
+            Console.WriteLine("RRR");
+        }
+    }
+    class Cat : Animal
+    {
+        public override void GetRRR()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Dog : Animal
+    {
+
+    }
+    class Rat : Animal
+    {
+
+    }
+}
