8454c1094432c1719085246c05e15ddbffa37877
AlexSemynin <alex.semynin97@mail.ru>
2019-02-11 01:46:18 +0300

    Создан делегат для создания событий. А для обработки событий создан класс. в котором аксессоры только для чтения и конструктор, при создании которого передаются сообщение и сумма в свойства приватные для записи. Но как раз через конструктор эта запись будет осуществляться

---
+++ 4_4_3_PRACTICA_OOP/BancLibrary/AccountStateHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancLibrary
+{
+    //Для реакции на изменения состояния счеты мы будем использовать событийную модель.
+    //Делегат:
+    public delegate void AccountStateHandler(object sender); // Делегат будет использоваться для создания событий
+    //вспомогательный класс
+    public class AccountEventsArgs  //для обработки событий
+    {
+        //аксессоры
+        public string Message { get; private set; } //Cообщение
+        public decimal Sum { get; private set; } //Cумма, на которую изменился счет
+
+        //конструктор
+        public AccountEventsArgs(string _mes, decimal _sum)
+        {
+            Sum = _sum;
+            Message = _mes;
+        }
+    }
+
+
+}
