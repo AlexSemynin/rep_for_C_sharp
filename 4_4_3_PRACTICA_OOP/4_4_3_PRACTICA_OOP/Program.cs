f23942b7dc54a4ffb8105bcf7809967cb249faa6
AlexSemynin <alex.semynin97@mail.ru>
2019-02-11 01:15:30 +0300

    ООП банковская задача. На этом шаге добавлен Class Library. Для хранения классов, интерфейсов и еще чего-нибудь)

---
+++ 4_4_3_PRACTICA_OOP/4_4_3_PRACTICA_OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_3_PRACTICA_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
