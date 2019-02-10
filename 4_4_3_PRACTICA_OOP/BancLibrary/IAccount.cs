a0acc69fc10304ae1902cbe689b3cea0ccde106d
AlexSemynin <alex.semynin97@mail.ru>
2019-02-11 01:25:02 +0300

    Добавлен интерфейс для ввода и вывода денег со счета

---
+++ 4_4_3_PRACTICA_OOP/BancLibrary/IAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancLibrary
+{
+    interface IAccount
+    {
+        void Put(decimal sum);         //положить деньги на счет
+        void GetMoney(decimal sum);    //взять
+    }
+}
