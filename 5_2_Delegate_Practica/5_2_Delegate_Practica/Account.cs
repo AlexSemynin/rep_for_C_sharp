83b3289b667c24ad248138a33c63b6ac23da6b8d
AlexSemynin <alex.semynin97@mail.ru>
2019-02-11 21:34:03 +0300

    Делегаты аккаунта. Вывод сообщения

---
+++ 5_2_Delegate_Practica/5_2_Delegate_Practica/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_Delegate_Practica
+{
+    class Account
+    {
+        //pole
+        private int _sum;
+        //Construct
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+        //Acsessor
+        public int CurrentSum { get { return _sum; } }
+        //Method
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+        public void Withdraw(int sum)
+        {
+            if (_sum >= sum)
+            {
+                _sum -= sum;
+                if(_del != null) { _del($"Сумма{sum} снята со счета"); }
+            }
+            else { if (_del != null) { _del("Недостаточно средств"); } }
+        }
+
+
+        //Объявление делегата 
+        public delegate void AccountStateHandler(string messege);
+        //Создание переменной делегата
+        private AccountStateHandler _del;
+        //Регистрация длегата
+        public void RegisterHAndler(AccountStateHandler del) //вызываемы метод, где в качестве параметра передаётся делегат 
+        {
+            _del = del;
+        }
+    }
+}
---
+++ 5_2_Delegate_Practica/5_2_Delegate_Practica/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_Delegate_Practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создаем банковскхий счет
+            Account acc = new Account(200);
+            //делегат передаем в качесве параметра в RegisterHandler
+            acc.RegisterHAndler(new Account.AccountStateHandler(ShowMessege)); //где в сам делегат добавляем ссылку на ShowMessege.
+            acc.Withdraw(100);
+            acc.Withdraw(200);
+            Console.ReadKey();
+
+        }
+        public static void ShowMessege(string mess)
+        {
+            Console.WriteLine(mess);
+        }
+    }
+}
