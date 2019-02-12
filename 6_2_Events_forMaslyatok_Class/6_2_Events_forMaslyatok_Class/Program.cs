fcf6da79279d26c2a6a85e8740c90249d6432c50
AlexSemynin <alex.semynin97@mail.ru>
2019-02-12 13:58:44 +0300

    Проект для работы с классом данных события

---
+++ 6_2_Events_forMaslyatok_Class/6_2_Events_forMaslyatok_Class/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2_Events_forMaslyatok_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(200);
+            account.Summed += Message; //Закрепляем за событием обработчик
+            account.Withdrown += Message;
+            account.Put(200);
+            account.Withdrow(300);
+            Console.ReadKey();
+        }
+        public static void Message(string mess)
+        {
+            Console.WriteLine(mess);
+        }
+        
+    }
+    
+    class Account
+    {
+        public delegate void MessageHandler(string str);
+        public event MessageHandler Summed;
+        public event MessageHandler Withdrown;
+        int _sum;
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+        public int Sum { get { return _sum; } }
+        public void Put(int sum)
+        {
+            _sum += sum;
+            Summed($"Пришло на счет {sum}");
+        }
+        public void Withdrow(int sum)
+        {
+            _sum -= sum;
+            Withdrown($"Списано {sum}");
+        }
+    }
+}
